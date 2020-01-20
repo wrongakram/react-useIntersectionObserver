87a8e3bdf531d1a95140be60687f105f3289f7e6
Akram Khalid <akramkhalid12@gmail.com>
2020-01-19 20:29:02 -0600

    starter files

--- src/App.js
+++ src/App.js
@@ -6,40 +6,6 @@ import gsap from "gsap";
 import "./App.scss";
 
 const App = () => {
-  // Ref for our element
-  const sectionRef = useRef(null);
-  // All the ref to be observed
-  const intersection = useIntersection(sectionRef, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.2
-  });
-
-  // Animation for fading in
-  const fadeIn = element => {
-    gsap.to(element, 1, {
-      opacity: 1,
-      y: -60,
-      ease: "power4.out",
-      stagger: {
-        amount: 0.3
-      }
-    });
-  };
-  // Animation for fading out
-  const fadeOut = element => {
-    gsap.to(element, 1, {
-      opacity: 0,
-      y: -20,
-      ease: "power4.out"
-    });
-  };
-
-  // checking to see when the vieport is visible to the user
-  intersection && intersection.intersectionRatio < 0.2
-    ? fadeOut(".fadeIn")
-    : fadeIn(".fadeIn");
-
   return (
     <>
       <div className='header'>
@@ -70,7 +36,7 @@ const App = () => {
         </p>
       </div>
       <div className='sectionSecond'>
-        <div ref={sectionRef} className='inner'>
+        <div className='inner'>
           <h3 className='fadeIn'>The talk of what makes a champion.</h3>
           <p className='fadeIn'>
             Massa id neque aliquam vestibulum. Nibh praesent tristique magna
