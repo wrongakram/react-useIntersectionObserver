5c77b207461c2d130ee7a40e211ecc3f1182f568
Akram Khalid <akramkhalid12@gmail.com>
2020-01-19 20:24:13 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { useIntersection } from "react-use";
+import back from "./left.svg";
+import send from "./send.svg";
+import gsap from "gsap";
+import "./App.scss";
 
-function App() {
+const App = () => {
+  // Ref for our element
+  const sectionRef = useRef(null);
+  // All the ref to be observed
+  const intersection = useIntersection(sectionRef, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.2
+  });
+
+  // Animation for fading in
+  const fadeIn = element => {
+    gsap.to(element, 1, {
+      opacity: 1,
+      y: -60,
+      ease: "power4.out",
+      stagger: {
+        amount: 0.3
+      }
+    });
+  };
+  // Animation for fading out
+  const fadeOut = element => {
+    gsap.to(element, 1, {
+      opacity: 0,
+      y: -20,
+      ease: "power4.out"
+    });
+  };
+
+  // checking to see when the vieport is visible to the user
+  intersection && intersection.intersectionRatio < 0.2
+    ? fadeOut(".fadeIn")
+    : fadeIn(".fadeIn");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className='header'>
+        <div className='sm-btn'>
+          <img src={back} alt='back arrow' />
+        </div>
+        <h5>About</h5>
+        <div className='sm-btn'>
+          <img src={send} alt='' />
+        </div>
+      </div>
+      <div className='sectionFirst'>
+        <div className='pic'>
+          <img
+            src='https://cdn.dribbble.com/users/997665/screenshots/9634478/media/8ad68f67fdbfb09bc054937edecf2d27.png'
+            alt=''
+          />
+        </div>
+        <h3>Alara Frank</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Massa id neque aliquam vestibulum. Nibh praesent tristique magna sit.
+          Auctor eu augue ut lectus arcu bibendum at varius. Nam aliquam sem et
+          tortor consequat id. Nunc mi ipsum faucibus vitae aliquet nec. Eu
+          consequat ac felis donec et. Vivamus arcu felis bibendum ut tristique.
+          Egestas diam in arcu cursus euismod quis viverra nibh. Donec ac odio
+          tempor orci dapibus ultrices in iaculis. Enim eu turpis egestas
+          pretium.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <div className='sectionSecond'>
+        <div ref={sectionRef} className='inner'>
+          <h3 className='fadeIn'>The talk of what makes a champion.</h3>
+          <p className='fadeIn'>
+            Massa id neque aliquam vestibulum. Nibh praesent tristique magna
+            sit. Auctor eu augue ut lectus arcu bibendum at varius. Nam aliquam
+            sem et tortor consequat id. Nunc mi ipsum faucibus vitae aliquet
+            nec. Eu consequat ac felis donec et. Vivamus arcu felis bibendum ut
+            tristique. Egestas diam in arcu cursus euismod quis viverra nibh.
+            Donec ac odio tempor orci dapibus ultrices in iaculis. Enim eu
+            turpis egestas pretium. Tortor vitae purus faucibus ornare
+            suspendisse sed nisi lacus sed. Eget nulla facilisi etiam dignissim
+            diam quis enim lobortis. Enim sit amet venenatis urna cursus eget.
+            Tellus id interdum velit laoreet id. Ac odio tempor orci dapibus. Et
+            ultrices neque ornare aenean euismod elementum nisi. Dolor morbi non
+            arcu risus quis. Lectus sit amet est placerat in egestas erat
+            imperdiet. Cum sociis natoque penatibus et magnis dis.
+          </p>
+          <div className='btn-row fadeIn'>
+            <a href='/'>Click here bruh</a>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
